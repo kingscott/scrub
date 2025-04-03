a22f83d7af55bce68629403413724b8906397acc
kingscott <me@kingscott.ca>
2025-04-03 18:34:56 -0400

    Update repo reference in boot

--- boot.sh
+++ boot.sh
@@ -32,7 +32,7 @@ sudo apt-get install -y git >/dev/null
 
 echo "Cloning omakub..."
 rm -rf ~/.local/share/omakub
-git clone https://github.com/kingscott/omakub.git ~/.local/share/omakub >/dev/null
+git clone https://github.com/kingscott/scrub.git ~/.local/share/omakub >/dev/null
 if [[ $OMAKUB_REF != "master" ]]; then
 	cd ~/.local/share/omakub
 	git fetch origin "${OMAKUB_REF:-stable}" && git checkout "${OMAKUB_REF:-stable}"
