24d211396a94f8428fe267a2e6dad1a47e688089
kingscott <me@kingscott.ca>
2025-04-03 20:54:38 -0400

    Fix ghostty theme

--- install/desktop/app-ghostty.sh
+++ install/desktop/app-ghostty.sh
@@ -4,4 +4,4 @@ snap install ghostty --classic
 
 # Add ghostty config
 mkdir -p ~/.config/ghostty
-cp ~/.local/share/omakub/configs/ghostty/config ~/.config/ghostty/
+cp -r ~/.local/share/omakub/configs/ghostty/ ~/.config/ghostty/
