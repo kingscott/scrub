593cd1a5d3bc054c09828e900958182b676f8b59
kingscott <me@kingscott.ca>
2025-04-05 15:59:00 -0400

    Force gnome-extensions install

--- install/desktop/set-gnome-extensions.sh
+++ install/desktop/set-gnome-extensions.sh
@@ -1,5 +1,5 @@
 sudo apt install -y gnome-shell-extension-manager pipx
-pipx install gnome-extensions-cli --system-site-packages
+pipx install gnome-extensions-cli --system-site-packages --force
 
 # Turn off default Ubuntu extensions
 gnome-extensions disable tiling-assistant@ubuntu.com
